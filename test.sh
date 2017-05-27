2ab2684b87b1e1281585b0c74d2a06a32ab75452
laixintao <glenn.lai@bosondata.com.cn>
2017-05-27 12:20:29 +0800

    add tmux-yorg

--- test.sh
+++ test.sh
@@ -1,21 +1,22 @@
 #!/bin/zsh
 
-MONGONAME='shandong'
-START_JOB='from yorg.contrib.court.shandong import start_job;start_job()'
+MONGONAME='ningxia'
+START_JOB='from yorg.contrib.court.ningxia.higher_court import start_job; start_job()'
 
 YORG_DIR='~/program/yorg/'
+TEST_YORG_PATH=~/program/test-yorg/
 cd $YORG_DIR
-echo you are now in ${YORG_DIR}
+echo you are now in `pwd`
 
 echo -n dropping datebase $MONGONAME ...
-python drop_mongo.py $MONGONAME
+python $TEST_YORG_PATH/drop_mongo.py $MONGONAME
 echo 'OK'
 
 PROCESS=`ps -ef|grep beanstalkd|grep -v grep|grep -v PPID|awk '{ print $2}'`
 for i in $PROCESS
 do
         echo "Kill the $1 process [ $i ]"
-        kill -9 $i
+        sudo kill -9 $i
 done
 
 echo -n run beanstalkd ...
@@ -23,9 +24,8 @@ beanstalkd &
 echo OK
 
 echo start_job ...
-python -c $START_JOB
+/home/laixintao/.virtualenvs/yorg/bin/python -c $START_JOB
 echo start_job finished
 
 echo start_yorg
-yorg
-
+/home/laixintao/.virtualenvs/yorg/bin/yorg
