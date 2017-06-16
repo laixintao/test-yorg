72ee9aa8bec193a23dc3b05aec27376930f4ca33
laixintao <glenn.lai@bosondata.com.cn>
2017-06-16 17:51:50 +0800

    template and rename file

---
+++ templates/chinacourt.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from yorg.utils import task_putter
+from yorg.utils import fully_qualified_func_name
+from yorg.contrib.court.chinacourt import get_page_num, parse_detail_page, parse_list_page
+from yorg.utils.decorators import queue_job
+
+URL_BASE = "http://jkxfy.chinacourt.org"
+COURT = "交口县人民法院"
+URL_LIST = "http://jkxfy.chinacourt.org/article/index/id/MzSoNDBINDAwNCACAAA%3D/page/{}.shtml"
+MONGO_NAME = 'shanxi'
+
+
+def do_parse_detail_page(url):
+    parse_detail_page(url, MONGO_NAME, COURT,
+                      get_start_time_by='content')
+
+
+def start_job():
+    total_page_num = get_page_num(URL_LIST.format(1))
+    for index in range(1, total_page_num + 1):
+        page_list_url = URL_LIST.format(index)
+        parse_list_page.enqueue(page_list_url, URL_BASE,
+                                fully_qualified_func_name(do_parse_detail_page))
---
+++ write_code.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""
+代码生成器：
+自动生成针对chinacourt域名下爬虫的测试代码。
+
+从template中根据模板生成代码，下载需要的测试文件，
+生成完成之后打印一条执行本测试的命令.
+我他妈简直是偷懒的天才.
+"""
+
+import os
+import click
+import requests
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option('--name', '-n', multiple=True)
+@click.option('--url', '-u')
+@click.option('--db-number', '-d', default=1)
+def test_chinacourt(name, url, db_number):
+    page_body = requests.get(url).text
+    if len(name) == 2:
+        package = "{}.{}".format(*name)
+        testname = "{}{}".format(*[n.capitalize() for n in name])
+        html_file_path = '/home/laixintao/program/yorg/tests/data/court/test_court_{}_{}_detail_page.html'.format(*name)
+        html_path = '"data/court/test_court_{}_{}_detail_page.html"'.format(*name)
+        test_path = '/home/laixintao/program/yorg/tests/court/test_{}_{}.py'.format(*name)
+    else:
+        package = name[0]
+        testname = name[0].capitalize()
+        html_path = '"data/court/test_court_{}_detail_page.html"'.format(*name)
+        html_file_path = '/home/laixintao/program/yorg/tests/data/court/test_court_{}_detail_page.html'.format(*name)
+        test_path = '/home/laixintao/program/yorg/tests/court/test_{}.py'.format(*name)
+    data_num = db_number
+    with open(html_file_path, 'wb') as html:
+        html.write(page_body.encode('utf-8'))
+    click.echo("Download html file success!")
+    with open('/home/laixintao/program/test-yorg/templates/test_chinacourt.py', 'r') as chinacourt:
+        content = chinacourt.read()
+        content = content.format(package=package, testname=testname, html_path=html_path, data_num=1, url=url)
+        with open(test_path, 'w') as test_file:
+            test_file.write(content)
+    click.echo("write code success!")
+    click.echo("pytest {}".format(test_path))
+
+
+@main.command()
+@click.option('--path', '-p')
+def chinacourt(path):
+    dir_path = '/home/laixintao/program/yorg/yorg/contrib/court/{}/'.format(path)
+    if not os.path.isdir(dir_path):
+        os.system('mkdir -p {}'.format(dir_path))
+    with open('/home/laixintao/program/test-yorg/templates/chinacourt.py', 'r') as chinacourt:
+        with open(dir_path+'__init__.py', 'w') as code:
+            code.write(chinacourt.read())
+
+
+if __name__ == '__main__':
+    main()
