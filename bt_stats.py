3467cc097bac41f968726b9247ee88c9fde3c738
laixintao <glenn.lai@bosondata.com.cn>
2017-08-30 15:10:36 +0800

    bt_stats: show more information and pretty

--- bt_stats.py
+++ bt_stats.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+
 import beanstalkc
+import msgpack
 
 bt = beanstalkc.Connection()
 
 tubes = bt.tubes()
 
+def get_jobs_from_tube(tube):
+    bt.watch(tube)
+    job = bt.reserve(timeout=0.01)
+    while job is not None:
+        yield job
+        job.bury()
+        job = bt.reserve(timeout=0.01)
+        
+
+print "total\tready\ttube"
 for tube in tubes:
-    print "{}\t{}".format(bt.stats_tube(tube)['total-jobs'], tube)
+    tube_status = bt.stats_tube(tube)
+    print "{}\t{}\t{}".format(tube_status['total-jobs'], tube_status['current-jobs-ready'], tube)
+
+    jobs = defaultdict(lambda :0)
+    for job in get_jobs_from_tube(tube):
+        task_data = msgpack.loads(job.body)
+        jobs[task_data['taskname']] += 1
+    for name, numbers in jobs.items():
+        print "\t{}\t  |-{}".format(numbers, name)
